98d48314e143f49e55f60d901c9b9730cad74272
pavsenin <pavsenin@mail.ru>
2018-04-04 16:17:22 +0300

    add webapp

---
+++ WebApp/FillOnex.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class FillOnex : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            API.receiveAndInsertBets(now);
+        }
+    }
+}
