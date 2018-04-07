975276a885739e41f9688c6e8ed545d89ea0d6ad
pavsenin <pavsenin@mail.ru>
2018-04-07 13:58:47 +0300

    retrive results

--- OneX.Core/API.fs
+++ OneX.Core/API.fs
@@ -4,6 +4,9 @@ open Utils
 open Domain
 open DBAdapter
 open FSharp.Data
+open FSharp.Data.JsonExtensions
+open FSharp.Data.HtmlAttribute
+open System
 
 let private receive time =
     champs
@@ -17,8 +20,51 @@ let receiveAndInsertBets connectionString time =
     matches |> insertNewMatches |> ignore
     bets |> insertNewBets |> ignore
 
+type Score = int * int
+type ScoreAdv = Score * Score list
+
+let getScore score =
+    let values = score |> splitNoEmpty [|':'|]
+    (Int32.Parse(values.[0]), Int32.Parse(values.[1]))
+
+let getScoreAdv scoreAdvString =
+    if String.IsNullOrEmpty scoreAdvString then None
+    else
+        let scoreSplitted = scoreAdvString |> splitNoEmpty [|'('; ')'|]
+        let score = scoreSplitted.[0]
+        let scoreTimes = scoreSplitted.[1] |> splitNoEmpty [|','|] |> Array.toList
+        Some (getScore score, scoreTimes |> List.map getScore)
+
+let getMatchResult (json:JsonValue) =
+    json.TryGetProperty("Head")
+    ||> (fun head ->
+        let scoreData = head.AsArray()
+        let scoreString = scoreData.[6].AsString()
+        let scoreOpt = getScoreAdv scoreString
+        scoreOpt |>> (fun score ->
+            let team1ID = scoreData.[22].AsInteger()
+            let team2ID = scoreData.[23].AsInteger()
+            let seconds = scoreData.[7].AsFloat()
+            let time = fromUnixTimestamp seconds
+            (team1ID, team2ID, time, score)
+        )
+    )
+
+let getLeagueResult json =
+    let id = json?ID.AsInteger()
+    let matches = json?Elems.AsArray() |> Array.choose getMatchResult
+    (id, matches)
 
 let receiveAndInsertResults connectionString date =
-    let x = getResults date
-    let y = x |> JsonValue.Parse
-    y
+    let response = getResults date
+    let json = response |> JsonValue.Parse
+    let data = json?Data.AsArray()
+    let footballElems = data |> Array.find (fun i -> i?ID.AsInteger() = 1)
+    let allLeagues = footballElems?Elems.AsArray()
+    let filteredLeagues = allLeagues |> Array.filter (fun l ->
+        let lID = l?ID.AsInteger()
+        champs |> List.tryFind (fun (_, id) -> lID = id) |> Option.isSome
+    )
+    let leagues = filteredLeagues |> Array.map getLeagueResult
+    //let results = leagues |> 
+    0
